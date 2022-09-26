c4eff03e75cab450ef21272b8ae873f973dff66e
João Marcus <joaomarcus75@gmail.com>
2022-09-26 12:49:57 -0300

    Assets and Scripts
    Script for walk and camera view, add test Character  and test animations

---
+++ The Rescue/Assets/Scripts/LookX.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookX : MonoBehaviour
+{
+    private float _sensitivity = 1f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float _mouseX = Input.GetAxis("Mouse X");
+
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.y += _mouseX * _sensitivity;
+        transform.localEulerAngles = newRotation;
+
+    }
+}
---
+++ The Rescue/Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacter : MonoBehaviour
+{
+    private CharacterController _controller;
+    private Animator _animator;
+    
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = -9.81f;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        CalculatedMovement();
+        
+    }
+
+    
+
+    public void CalculatedMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3  direction = new Vector3(horizontalInput, 0 ,verticalInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y = _gravity;
+        velocity = transform.transform.TransformDirection(velocity);
+        _controller.Move(velocity * Time.deltaTime);
+        if(direction != Vector3.zero)
+        {
+            _animator.SetBool("isMoving",true);
+        }
+        else
+        {
+            _animator.SetBool("isMoving",false);
+        }
+
+    
+    }
+
+
+}

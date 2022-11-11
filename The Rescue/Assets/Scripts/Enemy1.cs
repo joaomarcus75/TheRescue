80045110ca669ec9c8f471778532470ac47640af
João Marcus <joaomarcus75@gmail.com>
2022-11-11 14:52:50 -0300

    Assets & Script
     First Enemy Navmesh

--- The Rescue/Assets/Scripts/Enemy1.cs
+++ The Rescue/Assets/Scripts/Enemy1.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy1 : MonoBehaviour
 {
+
     private float _gravity = 1f;
     private float _temporaryVelocityY;
     private float _speed = 2f;
     
     private CharacterController _controller;
+    [SerializeField] private Transform movePositionTransform;
+    private NavMeshAgent _navMeshAgent;
     Vector3 direction;
     Vector3 velocity;
     
-    
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        
+    }
     void Start()
     {
        _controller = GetComponent<CharacterController>(); 
@@ -21,12 +29,14 @@ public class Enemy1 : MonoBehaviour
     
     void Update()
     {
-        velocity = direction * _speed;
-       _temporaryVelocityY -= _gravity;
-        velocity.y = _temporaryVelocityY;
-       velocity = transform.transform.TransformDirection( velocity);
+    //     velocity = direction * _speed;
+    //    _temporaryVelocityY -= _gravity;
+    //     velocity.y = _temporaryVelocityY;
+    //    velocity = transform.transform.TransformDirection( velocity);
        
-       _controller.Move(velocity * Time.deltaTime);
-      // direction = new Vector3(0,  1f ,0);
+    //    _controller.Move(velocity * Time.deltaTime);
+      
+
+       _navMeshAgent.destination = movePositionTransform.position;
     }
 }
